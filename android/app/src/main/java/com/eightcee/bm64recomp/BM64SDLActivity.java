356d4d674f4563430437d6e9daf301bc70186764
8cee <aycee0@gmail.com>
2026-09-25 16:10:34 +0200

    android: enforce immersive fullscreen

--- android/app/src/main/java/com/eightcee/bm64recomp/BM64SDLActivity.java
+++ android/app/src/main/java/com/eightcee/bm64recomp/BM64SDLActivity.java
@@ -10,7 +10,10 @@ import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import org.libsdl.app.SDLActivity;
 
@@ -65,6 +68,7 @@ public class BM64SDLActivity extends SDLActivity {
         }
 
         super.onCreate(savedInstanceState);
+        hideSystemUi();
         nativeConfigurePaths(programDir.getAbsolutePath(), dataDir.getAbsolutePath());
 
         VirtualPadView virtualPad = new VirtualPadView(this);
@@ -73,6 +77,38 @@ public class BM64SDLActivity extends SDLActivity {
                 ViewGroup.LayoutParams.MATCH_PARENT));
         addContentView(virtualPad, virtualPad.getLayoutParams());
         virtualPad.bringToFront();
+    }
+
+    private void hideSystemUi() {
+        final View decor = getWindow().getDecorView();
+        decor.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemUi();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUi();
     }
 
     public void openRomFilePicker() {
