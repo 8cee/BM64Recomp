5d83f0ff96317834ed6b48e48dda575cd9333a22
8cee <aycee0@gmail.com>
2026-09-25 03:22:17 +0200

    android: fix support header line breaks

--- include/zelda_support.h
+++ include/zelda_support.h
@@ -9,9 +9,13 @@
 
 namespace zelda64 {
     std::filesystem::path get_program_path();
-    std::filesystem::path get_asset_path(const char* asset);\n#ifdef __ANDROID__\n    std::filesystem::path get_android_app_folder_path();\n#endif
+    std::filesystem::path get_asset_path(const char* asset);
+#ifdef __ANDROID__
+    std::filesystem::path get_android_app_folder_path();
+#endif
     void open_file_dialog(std::function<void(bool success, const std::filesystem::path& path)> callback);
-    void open_file_dialog_multiple(std::function<void(bool success, const std::list<std::filesystem::path>& paths)> callback);\n    void open_mod_server(std::function<void(bool success, const std::list<std::filesystem::path>& paths)> callback);
+    void open_file_dialog_multiple(std::function<void(bool success, const std::list<std::filesystem::path>& paths)> callback);
+    void open_mod_server(std::function<void(bool success, const std::list<std::filesystem::path>& paths)> callback);
     void show_error_message_box(const char *title, const char *message);
 
 // Apple specific methods that usually require Objective-C. Implemented in support_apple.mm.
